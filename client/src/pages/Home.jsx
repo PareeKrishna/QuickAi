8c9ea9a669db73e503d08cb5ed34e3e34cd424c8
PareeKrishna <krishnapareek049@gmail.com>
2026-02-10 13:34:46 +0530

    Initial commit - QuickAi

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Hero from '../components/Hero.jsx'
+import AiTools from '../components/AiTools.jsx'
+
+const Home = () => {
+    return (
+        <>
+           <Navbar/> 
+           <Hero/>
+           <AiTools/>
+        </>
+    )
+}
+
+export default Home
