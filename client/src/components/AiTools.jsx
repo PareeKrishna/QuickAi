8be4a68b2e950f6543835873d8198a9dd69dd7d5
PareeKrishna <krishnapareek049@gmail.com>
2026-02-15 13:50:51 +0530

    QuickAi frontend completed

--- client/src/components/AiTools.jsx
+++ client/src/components/AiTools.jsx
@@ -18,11 +18,13 @@ const AiTools = () => {
             </p>
         </div>
 
-        <div className='flex flex-wrap mt-10 justify-between' >
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+
             {AiToolsData.map((tool,index)=>(
                 <div
                 key={index}
-                className="p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE] shadow-lg border border-gray-100 hover:-translate-y-1 transition-all duration-300 cursor-pointer"
+                className="p-8 rounded-lg bg-[#FDFDFE] shadow-lg border border-gray-100 hover:-translate-y-1 transition-all duration-300 cursor-pointer"
+
                 onClick={()=> user && navigate(tool.path)}
               >
                 <tool.Icon className='w-12 h-12 p-3 text-white rounded-xl' style={{background: `linear-gradient(to bottom, ${tool.bg.from}, ${tool.bg.to})`}} />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
 //import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//import { createRoot } from 'react-dom/client'
 import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
@@ -8,6 +8,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+console.log("Clerk key:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
