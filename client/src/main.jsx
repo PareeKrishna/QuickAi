0cc393e8dad08a0ccba8ae1a36c3bbdedaa54856
PareeKrishna <krishnapareek049@gmail.com>
2026-02-24 15:25:36 +0530

    QuickAi updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-console.log("Clerk key:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
+//console.log("Clerk key:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
