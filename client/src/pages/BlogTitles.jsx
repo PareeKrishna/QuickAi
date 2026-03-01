91fc6e2fbccd6d612ed1310949916836ee655104
PareeKrishna <krishnapareek049@gmail.com>
2026-03-01 21:46:34 +0530

    ui changes

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -46,7 +46,7 @@ const BlogTitles = () => {
         
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700' >
         {/* left col */}
-        <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200' >
+        <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200 border-t-4 border-t-purple-500' >
           <div className='flex items-center gap-3' >
             <Sparkles className='w-6 text-[#8E37EB]' />
             <h1 className='text-xl font-semibold' >AI Title Generator</h1>
@@ -72,7 +72,7 @@ const BlogTitles = () => {
           </button>
         </form>
         {/* right col */}
-        <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96' >
+        <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 border-t-4 border-t-purple-500 min-h-96' >
             <div className='flex items-center gap-3' >
               <Hash className="w-5 h-5 text-[#8E37EB]" />
               <h1 className='text-xl font-semibold' >Generated Titles</h1>
--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -48,7 +48,7 @@ const RemoveBackground = () => {
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700' >
         {/* left col */}
-        <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200' >
+        <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200 border-t-4 border-t-orange-500' >
           <div className='flex items-center gap-3' >
             <Sparkles className='w-6 text-[#FF4938]' />
             <h1 className='text-xl font-semibold' >Background Removal</h1>
@@ -68,7 +68,7 @@ const RemoveBackground = () => {
           </button>
         </form>
         {/* right col */}
-        <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px] overflow-hidden' >
+        <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 border-t-4 border-t-orange-500 min-h-96' >
             <div className='flex items-center gap-3' >
               <EraserIcon className="w-5 h-5 text-[#FF4938]" />
               <h1 className='text-xl font-semibold' >Processed Image</h1>
